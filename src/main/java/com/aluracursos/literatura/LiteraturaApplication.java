a4d4c5ea816e167a7c10ef59dc5e2c3fc580929d
SantiagoDEVSCI <santiago.aguilera.programacion@outlook.es>
2025-01-20 20:25:33 -0500

    se agrega el proyecto

---
+++ src/main/java/com/aluracursos/literatura/LiteraturaApplication.java
@@ -0,0 +1,30 @@
+package com.aluracursos.literatura;
+
+import com.aluracursos.literatura.Libreria.Libreria;
+import com.aluracursos.literatura.repository.iAutorRepository;
+import com.aluracursos.literatura.repository.iLibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaApplication implements CommandLineRunner {
+
+	@Autowired
+	private iLibroRepository libroRepository;
+	@Autowired
+	private iAutorRepository autorRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Libreria libreria = new Libreria(libroRepository, autorRepository);
+		libreria.consumo();
+
+	}
+}
---
+++ src/test/java/com/aluracursos/literatura/models/records/AutorTest.java
@@ -0,0 +1,20 @@
+package com.aluracursos.literatura.models.records;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutorTest {
+
+    @Test
+    void nombre() {
+    }
+
+    @Test
+    void cumpleanios() {
+    }
+
+    @Test
+    void fechaFallecimiento() {
+    }
+}
